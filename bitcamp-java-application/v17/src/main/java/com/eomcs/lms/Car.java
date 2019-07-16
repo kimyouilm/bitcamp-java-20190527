f22e16371142ec5cbebedc7357f5e6ccadd10f7d
kimyoulim <kimyoulim1016@gmail.com>
2019-07-16 10:40:43 +0900

    v17

---
+++ bitcamp-java-application/v17/src/main/java/com/eomcs/lms/Car.java
@@ -0,0 +1,18 @@
+package com.eomcs.lms;
+
+public class Car {
+
+  // static or class field
+  // => 클래스가 로딩될 때 Method Area에 자동 생성 된다.
+  // => 클래스는 딱 한번만 로딩되기 때문에(중복으로 로딩되지 않는다)
+  // 스태틱 필드도 딱 한 번만 생성된다.
+   static int count;
+//  int count;
+
+  // Instance field
+  // => new 명령어를 실행할 때 Heap에 생성된다.
+  // => new 명령을 실행하는 개수 만큼 인스턴스 필드가 생성된다.
+  int no;
+  String model;
+
+}
---
+++ bitcamp-java-application/v17/src/main/java/com/eomcs/lms/handler/MemberHandler.java
@@ -0,0 +1,43 @@
+package com.eomcs.lms.handler;
+
+import com.eomcs.lms.domain.Member;
+import com.eomcs.util.ArrayList;
+import com.eomcs.util.Input;
+
+public class MemberHandler {
+
+  private ArrayList memberList = new ArrayList();
+  private Input input;
+
+  public MemberHandler(Input input) {
+    this.input = input;
+  }
+
+  public void listMember() {
+
+    Object[] list = memberList.toArray();
+    for(Object obj : list) {
+      Member member = (Member) obj;
+      System.out.printf("%s, %s, %s, %s, %s, %s, %s\n\n", 
+          member.getNo(), member.getLectureName(), member.getEmail(), member.getPw(), 
+          member.getPic(), member.getPhoneNum(), member.getSignedUpDate());
+      
+    }
+  }
+
+  public void addMember() {
+    Member member = new Member();
+
+    member.setNo(input.getIntValue("번호 ?"));
+    member.setLectureName(input.getStringValue("이름? "));
+    member.setEmail(input.getStringValue("이메일? "));
+    member.setPw(input.getStringValue("암호? "));
+    member.setPic(input.getStringValue("사진? "));
+    member.setPhoneNum(input.getStringValue("전화? "));
+    member.setSignedUpDate(input.getDateValue("가입일? "));
+
+    memberList.add(member);
+    
+    System.out.println("저장하였습니다.");
+  }
+}
