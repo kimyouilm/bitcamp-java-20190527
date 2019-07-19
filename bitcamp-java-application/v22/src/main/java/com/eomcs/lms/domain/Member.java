66a410fa1d4874dad624712917b73527a4376b38
kimyoulim <kimyoulim1016@gmail.com>
2019-07-19 09:39:36 +0900

    v22

---
+++ bitcamp-java-application/v22/src/main/java/com/eomcs/lms/domain/Member.java
@@ -0,0 +1,58 @@
+package com.eomcs.lms.domain;
+
+import java.sql.Date;
+
+public class Member {
+
+  private int no;
+  private String name;
+  private String email;
+  private String pw;
+  private String pic;
+  private String phoneNum;
+  private Date signedUpDate;
+  public int getNo() {
+    return no;
+  }
+  public void setNo(int no) {
+    this.no = no;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getEmail() {
+    return email;
+  }
+  public void setEmail(String email) {
+    this.email = email;
+  }
+  public String getPw() {
+    return pw;
+  }
+  public void setPw(String pw) {
+    this.pw = pw;
+  }
+  public String getPic() {
+    return pic;
+  }
+  public void setPic(String pic) {
+    this.pic = pic;
+  }
+  public String getPhoneNum() {
+    return phoneNum;
+  }
+  public void setPhoneNum(String phoneNum) {
+    this.phoneNum = phoneNum;
+  }
+  public Date getSignedUpDate() {
+    return signedUpDate;
+  }
+  public void setSignedUpDate(Date signedUpDate) {
+    this.signedUpDate = signedUpDate;
+  }
+  
+  
+}
---
+++ bitcamp-java-application/v22/src/main/java/com/eomcs/util/QueueTest.java
@@ -0,0 +1,18 @@
+package com.eomcs.util;
+
+public class QueueTest {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    Queue<String> stack = new Queue<String>();
+    stack.offer("aaa");
+    stack.offer("bbb");
+    stack.offer("ccc");
+    stack.offer("ddd");
+    // 끝인지 아닌지 확인할때 
+
+    while (!stack.empty()) {
+      System.out.println(stack.poll());
+    }
+  }
+}
