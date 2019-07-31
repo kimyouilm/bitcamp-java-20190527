3eed4413c4125fa00fe055a0c7c8879c18f256fe
kimyoulim <kimyoulim1016@gmail.com>
2019-07-31 12:20:56 +0900

    observer

---
+++ bitcamp-java-basic/src/main/java/design_pattern/observer2/before/v2/Test.java
@@ -0,0 +1,20 @@
+// v2(임꺽정, 1월1일에 추가함): 텍스트 분석기에 Reader 연결
+package design_pattern.observer2.before.v2;
+
+import java.io.FileReader;
+
+public class Test {
+
+  public static void main(String[] args) {
+    // FillReader를 생성한놈이 닫는 것까지!
+    try (FileReader in = new FileReader("build.gradle")){
+      TextAnalyzer analyzer = new TextAnalyzer(in);
+      analyzer.execute();
+    } catch (Exception e) {
+      System.out.println("실행 중 오류 발생!");
+    }
+    
+
+  }
+
+}
