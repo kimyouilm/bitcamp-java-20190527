cfcc7cb9e46770cdfac502c544ddc26093124e4b
kimyoulim <kimyoulim1016@gmail.com>
2019-08-09 21:42:57 +0900

    pushpush

--- bitcamp-java-application-client2/src/main/java/com/eomcs/lms/dao/mariadb/BoardDaoImpl.java
+++ bitcamp-java-application-client2/src/main/java/com/eomcs/lms/dao/mariadb/BoardDaoImpl.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import com.eomcs.lms.dao.BoardDao;
 import com.eomcs.lms.domain.Board;
 
@@ -13,33 +14,33 @@ public class BoardDaoImpl implements BoardDao {
 
   @Override
   public int insert(Board board) throws Exception {
-    try (Connection con = DriverManager.getConnection(
-        "jdbc:mariadb://localhost/bitcampdb?user=bitcamp&password=1111");
+    try (
+        Connection con = DriverManager
+            .getConnection("jdbc:mariadb://localhost/bitcampdb?user=bitcamp&password=1111");
         Statement stmt = con.createStatement()) {
 
-      return stmt.executeUpdate(
-          "insert into lms_board(conts)"
-              + " values('" + board.getContents() + "')");
+      return stmt
+          .executeUpdate("insert into lms_board(conts)" + " values('" + board.getContents() + "')");
     }
   }
 
   @Override
   public List<Board> findAll() throws Exception {
-    try (Connection con = DriverManager.getConnection(
-        "jdbc:mariadb://localhost/bitcampdb?user=bitcamp&password=1111");
+    try (
+        Connection con = DriverManager
+            .getConnection("jdbc:mariadb://localhost/bitcampdb?user=bitcamp&password=1111");
         Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery(
-            "select * from lms_board order by board_id desc")) {
+        ResultSet rs = stmt.executeQuery("select * from lms_board order by board_id desc")) {
 
       ArrayList<Board> list = new ArrayList<>();
-      
+
       while (rs.next()) {
         Board board = new Board();
         board.setNo(rs.getInt("board_id"));
         board.setContents(rs.getString("conts"));
         board.setCreatedDate(rs.getDate("cdt"));
         board.setViewCount(rs.getInt("vw_cnt"));
-        
+
         list.add(board);
       }
       return list;
@@ -48,19 +49,55 @@ public class BoardDaoImpl implements BoardDao {
 
   @Override
   public Board findBy(int no) throws Exception {
-    // TODO Auto-generated method stub
-    return null;
+    try (
+        Connection con = DriverManager
+            .getConnection("jdbc:mariadb://localhost/bitcampdb?user=bitcamp&password=1111");
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from lms_board where board_id = " + no)) {
+
+      Board board = new Board();
+      board.setNo(rs.getInt("board_id"));
+      board.setContents(rs.getString("conts"));
+      board.setCreatedDate(rs.getDate("cdt"));
+      board.setViewCount(rs.getInt("vw_cnt"));
+      
+      return board;
+    }
   }
 
   @Override
   public int update(Board board) throws Exception {
-    // TODO Auto-generated method stub
-    return 0;
+    try (
+        Connection con = DriverManager
+            .getConnection("jdbc:mariadb://localhost/bitcampdb?user=bitcamp&password=1111");
+        Statement stmt = con.createStatement()) {
+
+
+      // update 문장은 executeUpdate()를 사용하여 서버에 전달한다.
+      return stmt.executeUpdate("update x_board'" + "', contents = '" + board.getContents() + "'"
+          + " where board_id = " + board.getNo());
+    }
   }
 
   @Override
   public int delete(int no) throws Exception {
-    // TODO Auto-generated method stub
+    try (Connection con = DriverManager.getConnection(
+        "jdbc:mariadb://localhost/bitcampdb?user=bitcamp&password=1111")) {
+      
+      try (Statement stmt = con.createStatement()) {
+        
+        // delete 문장은 executeUpdate()를 사용하여 서버에 전달한다.
+        int count = stmt.executeUpdate(
+            "delete from x_board where board_id = " + no);
+        
+        if (count == 0) {
+          System.out.println("해당 번호의 게시물이 존재하지 않습니다.");
+        } else {
+          System.out.println("삭제하였습니다.");
+        }
+      }
+      
+    }
     return 0;
   }
 
