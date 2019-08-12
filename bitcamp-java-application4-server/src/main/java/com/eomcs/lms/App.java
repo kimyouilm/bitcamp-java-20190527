507bf5b741d66ea47d4af70880e7fdb68cd3de87
kimyoulim <kimyoulim1016@gmail.com>
2019-08-12 17:57:45 +0900

    08122019

--- bitcamp-java-application4-server/src/main/java/com/eomcs/lms/App.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/App.java
@@ -1,6 +1,11 @@
-// client-v36_2 : DAO들이 Connection 객체를 공유하기. DBMS와의 연결 방식을 Stateful로 변경.
+// client-v37_1: '규칙1' 프로토콜에 따라 클라이언트 요청을 처리한다.
 package com.eomcs.lms;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.ArrayDeque;
@@ -36,120 +41,114 @@ import com.eomcs.util.Input;
 
 public class App {
 
-  Scanner keyScan;
+  private static final int CONTINUE = 1;
+  private static final int STOP = 0;
 
-  private void service() {
-    // DAO가 사용할 Connection 객체 준비하기
-    Connection con = null;
+  HashMap<String, Command> commandMap = new HashMap<>();
+  Connection con;
+
+  public App() throws Exception {
     try {
-      con = DriverManager.getConnection(
-        "jdbc:mariadb://localhost/bitcampdb?user=bitcamp&password=1111");
+      // DAO가 사용할 Connection 객체 준비하기
+      con = DriverManager
+          .getConnection("jdbc:mariadb://localhost/bitcampdb?user=bitcamp&password=1111");
+
+      // Command 객체가 사용할 데이터 처리 객체를 준비한다.
+      BoardDao boardDao = new BoardDaoImpl(con);
+      MemberDao memberDao = new MemberDaoImpl(con);
+      LessonDao lessonDao = new LessonDaoImpl(con);
+
+      // Client 명령을 처리할 커맨드 객체를 준비한다.
+      commandMap.put("/lesson/add", new LessonAddCommand(null, lessonDao));
+      commandMap.put("/lesson/delete", new LessonDeleteCommand(null, lessonDao));
+      commandMap.put("/lesson/detail", new LessonDetailCommand(null, lessonDao));
+      commandMap.put("/lesson/list", new LessonListCommand(null, lessonDao));
+      commandMap.put("/lesson/update", new LessonUpdateCommand(null, lessonDao));
+
+      commandMap.put("/member/add", new MemberAddCommand(null, memberDao));
+      commandMap.put("/member/delete", new MemberDeleteCommand(null, memberDao));
+      commandMap.put("/member/detail", new MemberDetailCommand(null, memberDao));
+      commandMap.put("/member/list", new MemberListCommand(null, memberDao));
+      commandMap.put("/member/update", new MemberUpdateCommand(null, memberDao));
+
+      commandMap.put("/board/add", new BoardAddCommand(null, boardDao));
+      commandMap.put("/board/delete", new BoardDeleteCommand(null, boardDao));
+      commandMap.put("/board/detail", new BoardDetailCommand(null, boardDao));
+      commandMap.put("/board/list", new BoardListCommand(null, boardDao));
+      commandMap.put("/board/update", new BoardUpdateCommand(null, boardDao));
+
     } catch (Exception e) {
       System.out.println("DBMS에 연결할 수 없습니다!");
-      return;
-    }
-    
-    // Command 객체가 사용할 데이터 처리 객체를 준비한다.
-    BoardDao boardDao = new BoardDaoImpl(con);
-    MemberDao memberDao = new MemberDaoImpl(con);
-    LessonDao lessonDao = new LessonDaoImpl(con);
-
-    keyScan = new Scanner(System.in);
-
-    Deque<String> commandStack = new ArrayDeque<>();
-    Queue<String> commandQueue = new LinkedList<>();
-
-    Input input = new Input(keyScan);
-
-    HashMap<String,Command> commandMap = new HashMap<>();
-
-    commandMap.put("/lesson/add", new LessonAddCommand(input, lessonDao));
-    commandMap.put("/lesson/delete", new LessonDeleteCommand(input, lessonDao));
-    commandMap.put("/lesson/detail", new LessonDetailCommand(input, lessonDao));
-    commandMap.put("/lesson/list", new LessonListCommand(input, lessonDao));
-    commandMap.put("/lesson/update", new LessonUpdateCommand(input, lessonDao));
-
-    commandMap.put("/member/add", new MemberAddCommand(input, memberDao));
-    commandMap.put("/member/delete", new MemberDeleteCommand(input, memberDao));
-    commandMap.put("/member/detail", new MemberDetailCommand(input, memberDao));
-    commandMap.put("/member/list", new MemberListCommand(input, memberDao));
-    commandMap.put("/member/update", new MemberUpdateCommand(input, memberDao));
-
-    commandMap.put("/board/add", new BoardAddCommand(input, boardDao));
-    commandMap.put("/board/delete", new BoardDeleteCommand(input, boardDao));
-    commandMap.put("/board/detail", new BoardDetailCommand(input, boardDao));
-    commandMap.put("/board/list", new BoardListCommand(input, boardDao));
-    commandMap.put("/board/update", new BoardUpdateCommand(input, boardDao));
-
-    while (true) {
-
-      String command = prompt();
-
-      if (command.length() == 0)
-        continue;
-
-      commandStack.push(command); 
-      commandQueue.offer(command); 
-
-      Command executor = commandMap.get(command);
-
-      if (command.equals("quit")) {
-        break;
-        
-      } else if (command.equals("history")) {
-        printCommandHistory(commandStack);
-
-      } else if (command.equals("history2")) {
-        printCommandHistory(commandQueue);
-
-      } else if (executor != null) {
-        executor.execute();
-
-      } else {
-        System.out.println("해당 명령을 지원하지 않습니다!");
-      }
-
-      System.out.println();
-    } //while
-    
-    // DBMS와의 연결을 끊는다.
-    try {
-      con.close();
-    } catch (Exception e) {
-      // 연결 끊을 때 발생되는 예외는 무시한다.
+      throw e;
     }
   }
 
-  private void printCommandHistory(Iterable<String> list) {
-    Iterator<String> iterator = list.iterator();
-    int count = 0;
-    while (iterator.hasNext()) {
-      System.out.println(iterator.next());
-      if (++count % 5 == 0) {
-        System.out.print(":");
-        if (keyScan.nextLine().equalsIgnoreCase("q"))
+  private void service() {
+    try (ServerSocket serverSocket = new ServerSocket(8888);) {
+      System.out.println("애플리케이션 서버가 시작되었음!");
+      while (true) {
+        if (processClient(serverSocket.accept()) == STOP)
           break;
-      }
+      } // while
+      
+      System.out.println("애플리케이션 서버를 종료함!");
+    }
+    // DBMS와의 연결을 끊는다.
+    catch (Exception e) {
+      System.out.println("소켓 통신오류");
+      e.printStackTrace();
     }
   }
 
-  private String prompt() {
-    System.out.print("명령> ");
-    return keyScan.nextLine();
+  private int processClient(Socket s) {
+    int state = CONTINUE;
+    try (Socket socket = s;
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintStream out = new PrintStream(socket.getOutputStream())) {
+
+      System.out.println("클라이언트와 연결됨!");
+
+      // return value;
+      while (true) {
+        // 클라이언트가 보낸 명령을 읽는다.
+        String request = in.readLine();
+        if (request.contentEquals("quit")) {
+          break;
+        } else if (request.equals("serverstop")) {
+          state = STOP;
+          break;
+        }
+        Command command = commandMap.get(request);
+
+        if (command == null) {
+          out.println("해당 명령을 처리할 수 없습니다.");
+          out.println("!end!");
+          out.flush();
+          continue;
+        } else {
+          command.execute(in, out);
+        }
+        out.println("!end!");
+        out.flush();
+      }
+      System.out.println("클라이언트와 연결 끊음!");
+
+      // 다른 클라이언트의 요청을 계속 처리할지 말지 상태값으로 알려준다.
+    } catch (Exception e) {
+      System.out.println("클라이언트와 통신 오류!");
+    }
+    return state;
+    
   }
-  
+
   public static void main(String[] args) {
-    App app = new App();
-    app.service();
+    try {
+      App app = new App();
+      app.service();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
-
-
-
-
-
-
-
-
 
 
--- bitcamp-java-application4-server/src/main/java/com/eomcs/lms/handler/Command.java
+++ bitcamp-java-application4-server/src/main/java/com/eomcs/lms/handler/Command.java
@@ -1,5 +1,16 @@
 package com.eomcs.lms.handler;
 
+import java.io.BufferedReader;
+import java.io.PrintStream;
+
 public interface Command {
-  void execute();
+  
+  // interface는 default를 남발하지 않는다.
+  // 기존규칙은 그대로 둔다.
+  default void execute() {}
+
+  // 새규칙을 추가한다.
+  // 단 기존에 이 인터페이스를 구현한 클래스에게 영향을 주지 않기 위해
+  // 새로 추가하는 메소드는 default메소드로 선언한다.
+  default void execute(BufferedReader in, PrintStream out) {}
 }
