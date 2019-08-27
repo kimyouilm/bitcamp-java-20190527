72637c6a18a69fde30035a329332e011c2467e0b
kimyoulim <kimyoulim1016@gmail.com>
2019-08-27 17:24:11 +0900

    hello?

--- bitcamp-java-basic/src/main/java/ch26/i/Test01.java
+++ bitcamp-java-basic/src/main/java/ch26/i/Test01.java
@@ -1,4 +1,4 @@
-// 트렌잭션 다루기 - commit() & rollback
+// 트렌잭션 다루기 - commit() & rollback 2) commit 호출전
 package ch26.i;
 
 import java.io.InputStream;
@@ -29,11 +29,11 @@ public class Test01 {
     board.setContents("내용");
     count = sqlSession.insert("board.insert", board);
     
-    sqlSession.commit();
     
     // insert 할 때 사용한 SqlSession으로  select를 실행한다면,
     //  커밋을 하지 안하도,
     // 기본적으로 임시 데이터베이스에 보관된 입력 데이터를 포함하여 결과를 리턴 한다.
+    // => 하지만 SqlSession을 닫으면 임시 보관된 데이터를 자동 제거된다.
     List<Board> boards = sqlSession.selectList("board.select");
     for (Board b : boards) {
       System.out.println(b);
---
+++ bitcamp-java-basic/src/main/java/ch26/i/Test02.java
@@ -0,0 +1,53 @@
+// 트렌잭션 다루기 - 여러 메소드에서 insert를 실행할 때
+package ch26.i;
+
+import java.io.InputStream;
+import java.util.List;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class Test02 {
+
+  public static void main(String[] args) throws Exception {
+
+    InputStream inputStream = Resources.getResourceAsStream("ch26/i/mybatis-config.xml");
+    SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+    // 서로다른 SqlSession으로 데이터 변경 작업(insert/update/delete)을 하면
+    // 같은 트랜젝션으로 다룰 수 없다.
+    insert1(sqlSessionFactory);
+    insert2(sqlSessionFactory);
+    printList(sqlSessionFactory);
+  }
+
+  static void insert1(SqlSessionFactory sqlSessionFactory) {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    sqlSession.insert("board.insert", new Board().setTitle("a103").setContents("내용"));
+    sqlSession.close();
+  }
+
+  static void insert2(SqlSessionFactory sqlSessionFactory) {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    Board board = new Board();
+    board.setTitle("a104");
+    board.setContents("내용");
+    sqlSession.insert("board.insert", board);
+
+    sqlSession.commit();
+    sqlSession.close();
+  }
+
+  static void printList(SqlSessionFactory sqlSessionFactory) {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    List<Board> boards = sqlSession.selectList("board.select");
+    for (Board b : boards) {
+      System.out.println(b);
+    }
+    sqlSession.commit();
+    sqlSession.close();
+  }
+}
+
+
